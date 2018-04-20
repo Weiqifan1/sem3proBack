f4de693bfbb728c1aaaac423bfc590ad6db00ef8
walter01git <26571249+walter01git@users.noreply.github.com>
2018-04-20 11:37:33 +0200

    Dommy data af reélle spisesteder

---
+++ frontend/src/Dummydata.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+
+const dummydata = [{
+    "name" : "Dominos",
+    "type" :  "fast food",
+    "price_range" : "cheap",
+    "number_of_reviews" : "00",
+    "url" : "dominos.dk"
+ },
+ {
+    "name" : "McDonalds",
+    "type" :  "fast food, family",
+    "price_range" : "cheap",
+    "number_of_reviews" : "00",
+    "url" : "www.mcdonalds.dk"
+ },
+ {
+    "name" : "Burger King",
+    "type" :  "fast food, family",
+    "price_range" : "cheap",
+    "number_of_reviews" : "00",
+    "url" : "www.burgerking.dk"
+ },
+ {
+    "name" : "Cafe Woody",
+    "type" :  "family, local",
+    "price_range" : "expensive",
+    "number_of_reviews" : "00",
+    "url" : "www.cafewoody.dk"
+ },
+ {
+    "name" : "Sticks'nSushi",
+    "type" :  "family, local, asian",
+    "price_range" : "expensive",
+    "number_of_reviews" : "00",
+    "url" : "www.sushi.dk"
+ },
+ {
+    "name" : "Big Mamma's Pizzaria",
+    "type" :  "fast food, family, local, pizzaria",
+    "price_range" : "average",
+    "number_of_reviews" : "00",
+    "url" : "www.bigmamma-pizzahouse.dk"
+ },
+ {
+    "name" : "Rådvad Kro",
+    "type" :  "luxus, family",
+    "price_range" : "expensive",
+    "number_of_reviews" : "00",
+    "url" : "www.raadvadkro.dk"
+ },
+ {
+    "name" : "Post-Pop Steakhouse",
+    "type" :  "local, luxus, steakhouse",
+    "price_range" : "expensive",
+    "number_of_reviews" : "00",
+    "url" : "www.postpub.dk/"
+ },
+ {
+    "name" : "Noma",
+    "type" :  "luxus, gourmet",
+    "price_range" : "very expensive",
+    "number_of_reviews" : "00",
+    "url" : "www.noma.dk"
+ },];
+
