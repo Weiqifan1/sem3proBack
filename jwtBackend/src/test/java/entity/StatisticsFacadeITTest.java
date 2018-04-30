17324f8035ad1c3a04bf45db096e940fd2610dd0
BenedikteEva <cph-bm109@cphbusiness.dk>
2018-04-30 20:05:15 +0200

    Merge origin/Developer into renderBasedOnrole
    Conflicts:
    	jwtBackend/src/main/java/entity/User.java
    	jwtBackend/target/classes/entity/User.class
    	jwtBackend/target/classes/rest/ApplicationConfig.class
    	jwtBackend/target/classes/security/LoginEndpoint.class
    	jwtBackend/target/jwtBackend-1.0-SNAPSHOT.war
    	jwtBackend/target/maven-archiver/pom.properties

---
+++ jwtBackend/src/test/java/entity/StatisticsFacadeITTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Ejer
+ */
+public class StatisticsFacadeITTest {
+    
+    public StatisticsFacadeITTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of addReviewsDatabaseAndApi method, of class StatisticsFacade.
+     */
+    @Test
+    public void testAddReviewsDatabaseAndApi() {
+        System.out.println("addReviewsDatabaseAndApi");
+        int a = 3;
+        int b = 6;
+        int expResult = 9;
+        int result = StatisticsFacade.addReviewsDatabaseAndApi(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+    
+    }
+    
+}
+
